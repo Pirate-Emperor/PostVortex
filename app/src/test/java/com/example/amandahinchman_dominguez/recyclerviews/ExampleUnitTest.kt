bffd21762e5f7e908db7cbecb9fcfa3c04ef3e7b
Pirate-Emperor <piratekingrahul@gmail.com>
2022-01-03 23:45:00 +0100

    Add app\src\test\java\com\example\amandahinchman_dominguez\recyclerviews\ExampleUnitTest.kt

---
+++ app/src/test/java/com/example/amandahinchman_dominguez/recyclerviews/ExampleUnitTest.kt
@@ -0,0 +1,16 @@
+// Pirate-Emperor - Author
+// This is a basic unit test class to verify functionality within a gaming application.
+
+package com.example.gamingapp
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+
+    // Test method to check if the addition operation is correct
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
